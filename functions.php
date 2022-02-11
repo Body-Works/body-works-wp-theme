786fb31c70cf97f62436386265ab48dee894147c
Konrad Fedorczyk <contact@realhe.ro>
2022-02-11 18:12:30 +0100

    Update theme

--- functions.php
+++ functions.php
@@ -7,7 +7,8 @@ define("TEMPLATE_VERSION", "1.0.0");
 add_action('wp_enqueue_scripts', 'body_works_enqueue_styles');
 function body_works_enqueue_styles()
 {
-	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css', [], TEMPLATE_VERSION);
+	// wp_enqueue_style('body-works-style', get_template_directory_uri() . '/style.css', [], TEMPLATE_VERSION);
+	wp_enqueue_style('twentysixteen-style', get_stylesheet_directory_uri() . "/parent.css");
 }
 
 /**
@@ -28,3 +29,11 @@ function wpdocs_child_theme_setup() {
     load_child_theme_textdomain( 'body_works', get_stylesheet_directory() . '/languages' );
 }
 add_action( 'after_setup_theme', 'wpdocs_child_theme_setup' );
+
+
+// parent-style-css
+add_action( 'wp_enqueue_scripts', 'mywptheme_child_deregister_styles', 11 );
+function mywptheme_child_deregister_styles() {
+    wp_dequeue_style( 'parent-style-css' );
+ 
+}
