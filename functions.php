ea1dcd17f0b71bcd33e9101a20735f2e23187012
Konrad Fedorczyk <contact@realhe.ro>
2022-02-15 16:12:11 +0100

    Fix empty cart flow

--- functions.php
+++ functions.php
@@ -73,7 +73,7 @@ function bw_woocommerce_header_add_to_cart_fragment($fragments)
 	<a class="cart-contents" href="<?php echo wc_get_cart_url(); ?>">
 		<div class="cart-icon-wrap">
 
-			<div class="bw_btn_primary engage-shake">
+			<div class="bw-btn-primary engage-shake">
 				<?= __("Evaluation", "body_works"); ?>
 			</div>
 
@@ -116,3 +116,16 @@ add_action('woocommerce_widget_shopping_cart_buttons', function () {
 add_action('woocommerce_widget_shopping_cart_buttons', function () {
 	echo '<a style="text-transform: uppercase !important" href="' . esc_url(wc_get_checkout_url()) . '" class="button checkout wc-forward">' . __('Evaluate the products', 'body_works') . '</a>';
 }, 20);
+
+/**
+ * Customize empty card message.
+ */
+remove_action('woocommerce_cart_is_empty', 'wc_empty_cart_message', 10);
+add_action('woocommerce_cart_is_empty', 'custom_empty_cart_message', 10);
+
+function custom_empty_cart_message()
+{
+	$html  = '<p class="cart-empty bw-custom-notification">';
+	$html .= wp_kses_post(apply_filters('wc_empty_cart_message', __('Please choose your items before a price evaluation.<br>You can do this by using product catalog:', 'body_works')));
+	echo $html . '</p>';
+}
