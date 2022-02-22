7ea38d24225840b6bb245d11fd240213eeddf7ac
Konrad Fedorczyk <contact@realhe.ro>
2022-02-22 17:52:24 +0100

    Add separator param to mail protection

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,12 @@ window.addEventListener("load", (event) => {
    * @link https://jsfiddle.net/fedek6/pzukv3so/
    */
   const emails = document.querySelectorAll("[data-email-protection='true']");
+  const separator = "[małpa]";
 
   for (const email of emails) {
     if (email.textContent) {
       const textContent = email.textContent;
-      const properEmail = textContent.replace("æ", "@");
+      const properEmail = textContent.replace(separator, "@");
       const replacement = document.createElement("a");
       replacement.setAttribute("href", "mailto:" + properEmail);
       replacement.textContent = properEmail;
