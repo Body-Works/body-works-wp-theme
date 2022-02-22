98d2f3f21207ace0b9a16e20dd31be7b9f5a37f5
Konrad Fedorczyk <contact@realhe.ro>
2022-02-22 16:38:32 +0100

    Add main js file for theme

--- functions.php
+++ functions.php
@@ -8,16 +8,16 @@ define("TEMPLATE_VARIANT", "body-works");
 add_action('wp_enqueue_scripts', 'body_works_enqueue_styles');
 function body_works_enqueue_styles()
 {
-	// wp_enqueue_style('body-works-style', get_template_directory_uri() . '/style.css', [], TEMPLATE_VERSION);
-	wp_enqueue_style('twentysixteen-style', get_stylesheet_directory_uri() . "/parent.css");
+  // wp_enqueue_style('body-works-style', get_template_directory_uri() . '/style.css', [], TEMPLATE_VERSION);
+  wp_enqueue_style('twentysixteen-style', get_stylesheet_directory_uri() . "/parent.css");
 }
 
 //
 function change_childtheme_stylesheet_ver()
 {
-	wp_dequeue_style('main-styles');
-	wp_deregister_style('main-styles');
-	wp_enqueue_style('main-styles', get_stylesheet_uri(), [], TEMPLATE_VERSION);
+  wp_dequeue_style('main-styles');
+  wp_deregister_style('main-styles');
+  wp_enqueue_style('main-styles', get_stylesheet_uri(), [], TEMPLATE_VERSION);
 }
 add_action('wp_print_styles', 'change_childtheme_stylesheet_ver');
 
@@ -27,9 +27,9 @@ add_action('wp_print_styles', 'change_childtheme_stylesheet_ver');
 add_action('wp', 'njengah_remove_zoom_effect_theme_support', 99);
 function njengah_remove_zoom_effect_theme_support()
 {
-	remove_theme_support('wc-product-gallery-zoom');
-	remove_theme_support('wc-product-gallery-lightbox');
-	remove_theme_support('wc-product-gallery-slider');
+  remove_theme_support('wc-product-gallery-zoom');
+  remove_theme_support('wc-product-gallery-lightbox');
+  remove_theme_support('wc-product-gallery-slider');
 }
 
 /**
@@ -37,7 +37,7 @@ function njengah_remove_zoom_effect_theme_support()
  */
 function wpdocs_child_theme_setup()
 {
-	load_child_theme_textdomain('body_works', get_stylesheet_directory() . '/languages');
+  load_child_theme_textdomain('body_works', get_stylesheet_directory() . '/languages');
 }
 add_action('after_setup_theme', 'wpdocs_child_theme_setup');
 
@@ -46,8 +46,15 @@ add_action('after_setup_theme', 'wpdocs_child_theme_setup');
 add_action('wp_enqueue_scripts', 'mywptheme_child_deregister_styles', 11);
 function mywptheme_child_deregister_styles()
 {
-	wp_dequeue_style('parent-style-css');
+  wp_dequeue_style('parent-style-css');
 }
+
+/**
+ * Add js to this theme
+ */
+add_action("wp_enqueue_scripts", function () {
+  wp_enqueue_script("bw-script", get_stylesheet_directory_uri() . '/assets/js/main.js', [], TEMPLATE_VERSION, true);
+});
 
 /**
  * Reorder templates in a single product view.
@@ -55,11 +62,11 @@ function mywptheme_child_deregister_styles()
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30);
 // Let's pack it in an additional div
 add_action('woocommerce_before_single_product_summary', function () {
-	echo "<div class='bw-custom-add-to-cart-wrapper'>";
+  echo "<div class='bw-custom-add-to-cart-wrapper'>";
 }, 20);
 add_action('woocommerce_before_single_product_summary', 'woocommerce_template_single_add_to_cart', 21);
 add_action('woocommerce_before_single_product_summary', function () {
-	echo "</div>";
+  echo "</div>";
 }, 22);
 
 
@@ -68,34 +75,34 @@ add_action('woocommerce_before_single_product_summary', function () {
  */
 function bw_woocommerce_header_add_to_cart_fragment($fragments)
 {
-	global $woocommerce;
+  global $woocommerce;
 
-	ob_start(); ?>
-	<a class="cart-contents" href="<?php echo wc_get_cart_url(); ?>">
-		<div class="cart-icon-wrap">
+  ob_start(); ?>
+  <a class="cart-contents" href="<?php echo wc_get_cart_url(); ?>">
+    <div class="cart-icon-wrap">
 
-			<div class="bw-btn-primary engage-shake">
-				<?= __("Evaluation", "body_works"); ?>
-			</div>
+      <div class="bw-btn-primary engage-shake">
+        <?= __("Evaluation", "body_works"); ?>
+      </div>
 
-			<div class="cart-wrap">
-				<span>
-					<?php echo $woocommerce->cart->cart_contents_count; ?>
-				</span>
-			</div>
-		</div>
-	</a>
+      <div class="cart-wrap">
+        <span>
+          <?php echo $woocommerce->cart->cart_contents_count; ?>
+        </span>
+      </div>
+    </div>
+  </a>
 <?php
 
-	$fragments['a.cart-contents'] = ob_get_clean();
+  $fragments['a.cart-contents'] = ob_get_clean();
 
-	return $fragments;
+  return $fragments;
 }
 
 // Remove parent filters
 add_action('after_setup_theme', function () {
-	remove_filter('woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment');
-	remove_filter('add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment');
+  remove_filter('woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment');
+  remove_filter('add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment');
 
   // Disable salient product listing
   remove_action('woocommerce_before_shop_loop_item_title', 'product_thumbnail_minimal');
@@ -109,14 +116,14 @@ add_filter('woocommerce_add_to_cart_fragments', 'bw_woocommerce_header_add_to_ca
  * Add a custom product listing.
  *
  */
-add_action('woocommerce_before_shop_loop_item_title', function() {
+add_action('woocommerce_before_shop_loop_item_title', function () {
   global $product;
   global $woocommerce;
   global $product_hover_alt_image;
   global $nectar_quick_view_in_use;
 
   require __DIR__ . '/custom-themes/product-thumbnail-minimal.php';
-}, 10 );
+}, 10);
 
 /**
  * Replace mini cart buttons
@@ -128,10 +135,10 @@ remove_action('woocommerce_widget_shopping_cart_buttons', 'woocommerce_widget_sh
 
 // Add custom ones
 add_action('woocommerce_widget_shopping_cart_buttons', function () {
-	echo '<a style="text-transform: uppercase !important" href="' . esc_url(wc_get_cart_url()) . '" class="button wc-forward">' . __('Selected items', 'body_works') . '</a>';
+  echo '<a style="text-transform: uppercase !important" href="' . esc_url(wc_get_cart_url()) . '" class="button wc-forward">' . __('Selected items', 'body_works') . '</a>';
 }, 10);
 add_action('woocommerce_widget_shopping_cart_buttons', function () {
-	echo '<a style="text-transform: uppercase !important" href="' . esc_url(wc_get_checkout_url()) . '" class="button checkout wc-forward">' . __('Evaluate the products', 'body_works') . '</a>';
+  echo '<a style="text-transform: uppercase !important" href="' . esc_url(wc_get_checkout_url()) . '" class="button checkout wc-forward">' . __('Evaluate the products', 'body_works') . '</a>';
 }, 20);
 
 /**
@@ -142,7 +149,7 @@ add_action('woocommerce_cart_is_empty', 'custom_empty_cart_message', 10);
 
 function custom_empty_cart_message()
 {
-	$html  = '<p class="cart-empty bw-custom-notification">';
-	$html .= wp_kses_post(apply_filters('wc_empty_cart_message', __('Please choose your items before a price evaluation.<br>You can do this by using product catalog:', 'body_works')));
-	echo $html . '</p>';
+  $html  = '<p class="cart-empty bw-custom-notification">';
+  $html .= wp_kses_post(apply_filters('wc_empty_cart_message', __('Please choose your items before a price evaluation.<br>You can do this by using product catalog:', 'body_works')));
+  echo $html . '</p>';
 }
