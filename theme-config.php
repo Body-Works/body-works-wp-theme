7e5f52e2dbe0a3047cbe6b64cbecc85f6f6fd12b
Konrad Fedorczyk <contact@realhe.ro>
2022-03-12 19:44:23 +0100

    Bump theme version

--- theme-config.php
+++ theme-config.php
@@ -8,7 +8,7 @@
  * @author Konrad Fedorczyk <contact@realhe.ro>
  * @link https://github.com/fedek6/body-works
  */
-define("TEMPLATE_VERSION", "1.0.34");
+define("TEMPLATE_VERSION", "1.0.35");
 define("TEMPLATE_VARIANT", "body-works");
 
 abstract class BwConfig {
